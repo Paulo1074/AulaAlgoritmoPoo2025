0c47d508de43e575742f1e1f6b5d72d7ae67bd69
Paulo Otávio <pereirapaulootavio98@gmail.com>
2025-02-18 19:37:58 -0300

    Atividade 1

--- Aula Faculdade/Program.cs
+++ Aula Faculdade/Program.cs
@@ -1,20 +1,20 @@
 ﻿double x1, x2, y1, y2, d, p1, p2;   
 { 
     {
-        Console.Write("Digite a coordenada x1: ");
+        Console.Write("Digite x1: ");
         x1 = int.Parse(Console.ReadLine());
 
-        Console.Write("Digite a coordenada y1: ");
+        Console.Write("Digite x2: ");
         y1 = int.Parse(Console.ReadLine());
 
-        Console.Write("Digite a coordenada x2: ");
+        Console.Write("Digite y1: ");
         x2 = int.Parse(Console.ReadLine());
 
-        Console.Write("Digite a coordenada y2: ");
+        Console.Write("Digite y2: ");
         y2 = int.Parse(Console.ReadLine());
 
         double distancia = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 
-        Console.WriteLine($"A distância entre os pontos ({x1}, {y1}) e ({x2}, {y2}) é: {distancia:F2}");
+        Console.WriteLine($"A distância entre os pontos ({x1}, {x2}) e ({y1}, {y2}) é: {distancia:F2}");
     }
 }
